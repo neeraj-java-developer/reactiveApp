66be7070af9f929f7dd76e0034d04a44f5d7f822
Neeraj Kumar <neerajkumar@Neerajs-MacBook-Pro.local>
2023-07-20 21:10:01 +0530

    first commit

---
+++ reactive-api/src/main/java/com/neeraj/reactiveapi/Controller/UserController.java
@@ -0,0 +1,46 @@
+package com.neeraj.reactiveapi.Controller;
+
+
+import com.neeraj.reactiveapi.Dto.Foo;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.logging.Logger;
+
+@RestController
+public class UserController {
+
+    private static final Logger logger = Logger.getLogger(UserController.class.getName());
+
+
+    @GetMapping(value = "/foos", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Foo> getResourcesByInterval() {
+
+        logger.info("api calling - foos");
+
+        return Flux.interval(Duration.ofSeconds(1))
+                .map(sequence -> new Foo(sequence.intValue(),"Foo #" + sequence));
+    }
+
+    @GetMapping("/helloMono")
+    public Mono<String> helloMono() {
+        logger.info("calling simple api for saying hello using mono");
+        return Mono.just("Hello, reactive!");
+    }
+
+    @GetMapping(value = "/helloFlux", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> helloFlux() {
+
+        logger.info("calling simple api for just flex");
+        Flux<String> flux = Flux.just("A", "B", "C");
+
+        return flux;
+
+
+
+    }
+}
